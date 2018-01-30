4f7d81b5340c4121efde91d9ccb7b4d75bb853e5
Alex Bril <affabilis@yandex.ru>
2018-01-30 23:06:25 +0300

    dohrena of bundles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -24,7 +25,9 @@ class ArticleController
      */
     public function show(String $slug)
     {
-        return new Response(sprintf("Second page for %s", $slug));
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug ))
+        ]);
     }
 
 
